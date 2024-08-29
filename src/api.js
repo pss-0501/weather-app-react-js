78edad98802284f6b279926c9e316d9adde2d818
pss-0501 <psawant507@gmail.com>
2024-08-28 20:36:28 -0400

    search

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "f3efd109f2mshcf24e59a92c8374p1c74e2jsnbf1183a7a2ee",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+  export const WEATHER_API_KEY = "bf585fa1e4c5ee24ce96fc2f460e60c1";
